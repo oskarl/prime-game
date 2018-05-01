0f54a23417ee644dbfed1e72485d48680e528c82
oskarl <oskar.larsson@onewave.se>
2018-05-01 12:32:44 +0200

    sw

--- js/app.js
+++ js/app.js
@@ -19,8 +19,8 @@ for(var i = 2; i < 10000; i++)
 }
 
 if (!navigator.serviceWorker.controller) {
-	navigator.serviceWorker.register('js/sw.js', {
-		scope: '/'
+	navigator.serviceWorker.register('sw.js', {
+		scope: './'
 	}).then(function(reg) {
 		console.log('Service worker has been registered for scope:'+ reg.scope);
 	});
